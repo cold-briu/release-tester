ea64ecc74cdd235a1d68257099b1edf6f170471c
andresf.xyz <andres.xyz@gmail.com>
2023-04-17 18:21:12 -0500

    fix: bypass input check

--- .github/workflows/scripts/lint-changelog.sh
+++ .github/workflows/scripts/lint-changelog.sh
@@ -3,12 +3,6 @@
 # get the input parameter
 raw_release_notes=$1
 
-# Check if the file exists
-if [ ! -f "$1" ]; then
-  echo "File not found: $1"
-  exit 1
-fi
-
 # Check if the file contains the expected structure
 if ! grep -q '^new features:' "$1"; then
   echo "Expected 'new features: ' section not found in file: $1"
