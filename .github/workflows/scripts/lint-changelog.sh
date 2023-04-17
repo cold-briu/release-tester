6030bba66a016afd98dcaecf4a7c43dd62c58fdf
andresf.xyz <andres.xyz@gmail.com>
2023-04-17 18:32:08 -0500

    feat; updated linter to woork with strings

--- .github/workflows/scripts/lint-changelog.sh
+++ .github/workflows/scripts/lint-changelog.sh
@@ -3,25 +3,22 @@
 # get the input parameter
 raw_release_notes=$1
 
-# Check if the file contains the expected structure
-if ! grep -q '^new features:' "$1"; then
-  echo "Expected 'new features: ' section not found in file: $1"
+# Check if the string contains the expected structure
+if [[ ! "$raw_release_notes" =~ ^new\ features: ]]; then
+  echo "Expected 'new features: ' section not found in release notes"
   exit 1
 fi
 
-if ! grep -q '^breaking changes:' "$1"; then
-  echo "Expected 'breaking changes: ' section not found in file: $1"
+if [[ ! "$raw_release_notes" =~ ^breaking\ changes: ]]; then
+  echo "Expected 'breaking changes: ' section not found in release notes"
   exit 1
 fi
 
-if ! grep -q '^fixes:' "$1"; then
-  echo "Expected 'fixes: ' section not found in file: $1"
+if [[ ! "$raw_release_notes" =~ ^fixes: ]]; then
+  echo "Expected 'fixes: ' section not found in release notes"
   exit 1
 fi
 
-
-# bypass:  remove line breaks
-linted_release_notes=$raw_release_notes
 
 # output the linted release notes
-echo "${linted_release_notes}"
+echo "${raw_release_notes}"
